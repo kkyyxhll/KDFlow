0b20bc23ec37d8fcf61980fb0d2801b0fc31843f
songmzhang <787073341@qq.com>
2026-08-06 14:30:46 +0800

    refactor(rollout): decouple generation and data processing from trainer

--- kdflow/trainer/__init__.py
+++ kdflow/trainer/__init__.py
@@ -1,9 +1,11 @@
 from kdflow.trainer.sft_trainer import SFTTrainer
 from kdflow.trainer.on_policy_kd_trainer import OnPolicyKDTrainer
 from kdflow.trainer.off_policy_kd_trainer import OffPolicyKDTrainer
+from kdflow.trainer.rollout_manager import RolloutManager
 
 __all__ = [
     "SFTTrainer",
     "OffPolicyKDTrainer",
     "OnPolicyKDTrainer",
+    "RolloutManager",
 ]
